aa0d6309fc529c1811cda63828fb09f9ffddf8cf
Juanca Ojeda <taraguipora2005@gmail.com>
2022-08-12 21:42:28 -0300

    Ejercicio 7 Python valoracion final modulo

---
+++ ejercicio 7.py
@@ -0,0 +1,14 @@
+Historial3 = (9530,4120,4580,1500,890,7516,426)
+suma= 0
+i =0
+    
+for elemento in Historial3:
+    suma += elemento
+    i += 1
+
+promedio = suma / i
+print (promedio)
+
+if promedio >  4500:
+    print ("SE A EXECIDO DEL GASTO PROMEDIO PARA SU MASCOTA")
+    
