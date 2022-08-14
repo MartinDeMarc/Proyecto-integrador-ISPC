05f386e7d9cb9cf289df2b69932b4482fd0cc799
Jose Zaragoza <87447700+JoseZaragoza7@users.noreply.github.com>
2022-08-13 21:07:08 -0300

    Ejercicio 5 Python

---
+++ Tupla Historial.py
@@ -0,0 +1,16 @@
+""" 5)      Crear una tupla en Python con el nombre de “Historial” la cual contenga los siguientes valores:
+
+          2350, 5960, 23000, 1000, 8900
+
+    Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota “Puppy”. 
+    Calcular el monto total gastado a lo largo del tiempo por atención de “Puppy”. Si el gasto efectuado 
+    es menor a 30000, mostrar en pantalla dicho resultado,
+     caso contrario mostrar el mensaje “Gastos por encima de lo presupuestado”. """
+     
+Historial=(2350,5960,23000,1000,8900)
+Puppy = sum (Historial)
+if Puppy <= 30000 :
+    print (Puppy)
+else :
+    print("Gastos por encima de lo presupuestado")
+
