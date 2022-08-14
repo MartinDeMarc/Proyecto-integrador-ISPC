41d8c113d12540aec4b2c12e218d4a9978d92f17
anahinatale <nataleanahi@gmail.com>
2022-08-14 00:42:13 -0300

    Reorganizando archivos

--- Historial4.py
+++
@@ -1,8 +0,0 @@
-Historial4 = (7510, 7960, 76180, 800, 4100);
-valor_min = Historial4[0];
-
-for i in Historial4:
-    if valor_min > i:
-        valor_min = i;
-        
-print("El valor mínimo de atención gastada en Olivia es: $", valor_min) 
